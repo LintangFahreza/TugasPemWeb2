0318549cc2ec81920d26cfdd1aa0c58499c6b5fe
lintangfahreza <lintangfahreza.lf@gmail.com>
2024-02-29 23:18:45 +0700

    add tugas pertemuan1

---
+++ Pertemuan 1/latihan1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Latihan1</title>
+</head>
+<body>
+<?php
+echo"<pre>";
+echo "<div style='text-align: center; font-size: 28px; color: red;'><b>BIODATA</b></div>";
+echo "<div style='font-size: 24px; color: blue;'>
+                                 Nama:    <b>Achmad Riki</b>
+                                 Email:   ariki@yahoo.com
+                                 Jurusan: Teknik Informatika
+                                 Hobi:    Travelling
+      </div>";
+?>
+</body>
+</html>
---
+++ Pertemuan 1/latihan2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan2</title>
+</head>
+<body>
+    <?php 
+    $nama = "Muhammad Lintang Fahreza";
+    $smt = 6;
+    $jurusan = "Teknik Informatika";
+    $unv= "Universitas Pamulang";
+    echo "<pre>";
+    echo "<hr size='4' width='100%' color='blue'>";
+    echo "<center><h1 style='color: red;'>HALLO SAYA BARU BELAJAR PEMROGRAMAN PHP</h1></center>";
+    echo "<hr size='4' width='100%' color='blue'>";
+    echo "<p>Nama saya <b>$nama</b></p>";
+    echo "<p>Saya berkuliah di $unv</p>";
+    echo "<p>Sekarang saya sudah berada di tingkat semester $smt</p>";
+    ?>
+</body>
+</html>
