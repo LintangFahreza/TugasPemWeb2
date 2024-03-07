bd8c61366a640126f37e18dce3c1bc0731933116
lintangfahreza <lintangfahreza.lf@gmail.com>
2024-03-07 15:02:27 +0700

    add pertemuan2 perbaikan

--- Pertemuan 2/latihan 2/index.php
+++ Pertemuan 2/latihan 2/index.php
@@ -1,61 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pemweb2 | Pertemuan 2 | Latihan 2</title>
     <style>
-        label{
+        label {
             margin-right: 179px;
             color: red;
             font-weight: bold;
         }
     </style>
 </head>
+
 <body>
-<h1>Latihan 2</h1>
-<form action="" method="post">
-    <label for="nilai1">Nilai I</label><label for="nilai2">Nilai II</label><br>
-    <input type="number" name="nilai1" required>
+    <h1>Latihan 2</h1>
+    <form action="" method="post">
+        <label for="nilai1">Nilai I</label><label for="nilai2">Nilai II</label><br>
+        <input type="number" name="nilai1" required>
 
-    <select name="operasi" required>
-        <option value="tambah">(+)</option>
-        <option value="kurang">(-)</option>
-        <option value="kali">(x)</option>
-        <option value="bagi">(/)</option>
-    </select>
-    <input type="number" name="nilai2" required>
+        <select name="operasi" required>
+            <option value="tambah">(+)</option>
+            <option value="kurang">(-)</option>
+            <option value="kali">(x)</option>
+            <option value="bagi">(/)</option>
+        </select>
+        <input type="number" name="nilai2" required>
 
-    <button type="submit">Hitung</button>
-</form>
+        <button type="submit">Hitung</button>
+    </form>
 
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $nilai1 = $_POST['nilai1'];
-    $nilai2 = $_POST['nilai2'];
-    $operasi = $_POST['operasi'];
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $nilai1 = $_POST['nilai1'];
+        $nilai2 = $_POST['nilai2'];
+        $operasi = $_POST['operasi'];
 
-    switch ($operasi) {
-        case 'tambah':
+        if ($operasi == 'tambah') {
             $hasil = $nilai1 + $nilai2;
-            break;
-        case 'kurang':
+        } elseif ($operasi == 'kurang') {
             $hasil = $nilai1 - $nilai2;
-            break;
-        case 'kali':
-            $hasil = $nilai1 * $nilai2; 
-            break;
-        case 'bagi':
+        } elseif ($operasi == 'kali') {
+            $hasil = $nilai1 * $nilai2;
+        } elseif ($operasi == 'bagi') {
             $hasil = ($nilai2 != 0) ? $nilai1 / $nilai2 : "Error: Pembagian dengan nol tidak diperbolehkan.";
-            break;
-        default:
+        } else {
             $hasil = "Operasi tidak valid.";
-            break;
+        }
+        echo "<br><b>Hasil perhitungan:</b> $hasil";
     }
-
-    echo "<br><b>Hasil perhitungan:</b> $hasil";
-}
-?>
+    ?>
 
 </body>
+
 </html>
